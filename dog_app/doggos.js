aa16950ff5f0bdcffb29bc11592e706694c1955f
texalana <79461309+texalana@users.noreply.github.com>
2024-08-30 01:31:34 +0200

    Finish styling

--- dog_app/doggos.js
+++ dog_app/doggos.js
@@ -1,29 +1,3 @@
-// const DOG_URL = "https://dog.ceo/api/breeds/image/random";
-
-
-
-// const doggos = document.querySelector(".doggos");
-
-// function addNewDoggo() {
-//     // show loading spinner 
-//     const promise = fetch(DOG_URL);
-//     promise
-//       .then(function(response) {
-//         const processingPromise = response.json();
-//         return processingPromise;
-//       })
-//       .then(function(processedResponse) {
-//         const img = document.createElement("img");
-//         img.src = processedResponse.message;
-//         img.alt = "Cute doggo";
-//         doggos.appendChild(img);
-
-//         // stop showing the spinner
-//       });
-//   }
-  
-//   document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
-
 
 const BREEDS_URL = "https://dog.ceo/api/breeds/list/all"
   const select = document.querySelector(".breeds")
